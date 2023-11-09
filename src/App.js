b03bf9e98af4c53493e8dd7a2be5d552dbc3b62c
ZiadSheriif <zsherif308@gmail.com>
2023-11-09 16:19:12 +0200

    feat: Add initial code strcuture

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Egyptian Premier League</h1>
+      </header>
+
+      <section>
+        <h2>Team Standings</h2>
+      </section>
+    </div>
+  );
+}
+
+export default App;
