b86e7d049280c4ed627ee2be7709b302a935b8b2
ZiadSheriif <zsherif308@gmail.com>
2023-11-27 01:56:02 +0200

    feat: Add toggling of theme

---
+++ src/Theme/darkTheme.js
@@ -0,0 +1,31 @@
+/**
+ * dark theme colors
+ *
+ * @type {{id:string}}
+ */
+const darkTheme = {
+  // The id of the theme
+  id: "dark",
+
+  // The colors of the text on the website
+  color: { primary: "#FF0000" },
+  // The colors of the backgrounds in the website
+  background: {
+    primary: "#282c34;",
+  },
+  // Border colors
+  borderColor: {},
+  // The colors of the lines in the website
+  lineColor: {},
+  // The colors of the buttons in the website
+  button: {},
+  arrowColor: {},
+  btnColor: {},
+  categoryColor: {},
+  reportModalExp: {},
+  degree: {
+    gradient: 180,
+  },
+};
+
+export default darkTheme;
---
+++ src/Theme/lightTheme.js
@@ -0,0 +1,44 @@
+/**
+ * light theme colors
+ *
+ * @type {{id:string}}
+ */
+const lightTheme = {
+  // The id of the theme
+  id: "light",
+
+  // The colors of the text on the website
+  color: {
+    primary: "#61dafb",
+  },
+  // The colors of the backgrounds in the website
+  background: {
+    primary: "#f2f2f2",
+    hoverOfNav: "#414754",
+  },
+  // Border colors
+  borderColor: {
+    // Primary border color
+    primary: "#878a8c",
+  },
+  // The colors of the lines in the website
+  lineColor: {
+    primary: "#edeff1",
+  },
+  // The colors of the buttons in the website
+  button: {
+    //The border color of hover card
+    secondary: "#f0f0f0",
+  },
+  // The colors of the arrows in the leadboard
+  arrowColor: {},
+  btnColor: {
+    primary: "#0079d3",
+  },
+  categoryColor: {},
+  reportModalExp: {},
+  //To change Linear Gradient Degree according to theme
+  degree: {},
+};
+
+export default lightTheme;
