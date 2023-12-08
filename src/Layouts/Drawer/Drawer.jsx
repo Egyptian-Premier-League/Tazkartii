60334a43fa4e58c0a6bf61e3789b63e01919ef86
ZiadSheriif <zsherif308@gmail.com>
2023-12-08 16:01:31 +0200

    feat: Add sidebar having forms of managers

--- src/Layouts/Drawer/Drawer.jsx
+++ src/Layouts/Drawer/Drawer.jsx
@@ -1,70 +1,46 @@
-import React from "react";
-import { Drawer as MUIDrawer, List, ListItem, ListItemIcon, ListItemText } from "@mui/material";
-import { Link } from "react-router-dom";
-import HomeIcon from "@mui/icons-material/Home";
-import InfoIcon from "@mui/icons-material/Info";
-import ContactMailIcon from "@mui/icons-material/ContactMail";
-// import AppBar from "@mui/material/AppBar";
-// import Toolbar from "@mui/material/Toolbar";
-// import IconButton from "@mui/material/IconButton";
-// import MenuIcon from "@mui/icons-material/Menu";
-// import Typography from "@mui/material/Typography";
+import React, { useState } from "react";
+import {
+  SidebarContainer,
+  MenuList,
+  MenuItem,
+  DrawerButton,
+  Overlay,
+  MdChevronRightStyled,
+  MdChevronLeftStyled,
+  IconWrapper,
+} from "./Drawer.styled";
+import { useNavigate } from "react-router-dom";
 
-const Drawer = ({ isOpen, toggleDrawer }) => {
+const Drawer = () => {
+  const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDrawer = () => setIsOpen(!isOpen);
+
+  const handleStadiumCreate = () => {
+    setIsOpen(false);
+    navigate("/add-stadium");
+  };
+
+  const handleMatchCreate = () => {
+    setIsOpen(false);
+    navigate("/add-match");
+  };
+
   return (
-    <MUIDrawer anchor="left" open={isOpen} onClose={toggleDrawer(false)}>
-      <List>
-        {/* Link to Home */}
-        <ListItem component={Link} to="/" onClick={toggleDrawer(false)}>
-          <ListItemIcon>
-            <HomeIcon />
-          </ListItemIcon>
-          <ListItemText primary="Home" />
-        </ListItem>
-
-        {/* Link to About */}
-        <ListItem component={Link} to="/about" onClick={toggleDrawer(false)}>
-          <ListItemIcon>
-            <InfoIcon />
-          </ListItemIcon>
-          <ListItemText primary="About" />
-        </ListItem>
-
-        {/* Link to Contact */}
-        <ListItem component={Link} to="/contact" onClick={toggleDrawer(false)}>
-          <ListItemIcon>
-            <ContactMailIcon />
-          </ListItemIcon>
-          <ListItemText primary="Contact" />
-        </ListItem>
-      </List>
-    </MUIDrawer>
+    <>
+      <Overlay isOpen={isOpen} onClick={toggleDrawer} />
+      <SidebarContainer isOpen={isOpen}>
+        <DrawerButton onClick={toggleDrawer} isOpen={isOpen}>
+        <IconWrapper>
+          {isOpen ? <MdChevronLeftStyled size={30} /> : <MdChevronRightStyled size={30} />}</IconWrapper>
+        </DrawerButton>
+        <MenuList>
+          <MenuItem onClick={handleStadiumCreate}>Create Stadium</MenuItem>
+          <MenuItem onClick={handleMatchCreate}>Create Match</MenuItem>
+        </MenuList>
+      </SidebarContainer>
+    </>
   );
 };
-
 export default Drawer;
-
-// const [drawerOpen, setDrawerOpen] = useState(false);
-
-// const toggleDrawer = (open) => (event) => {
-//   if (event.type === "keydown" && (event.key === "Tab" || event.key === "Shift")) {
-//     return;
-//   }
-//   setDrawerOpen(open);
-// };
-// {
-/* <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="menu" onClick={toggleDrawer(true)}>
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" style={{ flexGrow: 1 }}>
-            My App
-          </Typography>
-        </Toolbar>
-      </AppBar> */
-// }
-
-// {
-//   /* <Drawer isOpen={drawerOpen} toggleDrawer={toggleDrawer} /> */
-// }
---
+++ src/Layouts/Drawer/Drawer.styled.js
@@ -0,0 +1,80 @@
+import styled from "styled-components";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+
+export const SidebarContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: ${({ isOpen }) => (isOpen ? "0" : "-250px")};
+  width: 250px;
+  height: 100%;
+  background-color: #282c34;
+  transition: left 0.3s ease;
+  z-index: 1500;
+`;
+
+export const MenuList = styled.ul`
+  list-style: none;
+  padding: 20px;
+`;
+
+export const MenuItem = styled.li`
+  padding: 10px;
+  margin: 10px 0;
+  background-color: #007bff;
+  color: white;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
+
+export const DrawerButton = styled.button`
+  position: absolute;
+  top: 20px;
+  right: ${({ isOpen }) => (isOpen ? "-40px" : "-25px")};
+  width: 35px;
+  height: 35px;
+  background-color: #282c34;
+  color: white;
+  text-align: center;
+  border-radius: 50%;
+  cursor: pointer;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+  border: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Overlay = styled.div`
+  display: ${({ isOpen }) => (isOpen ? "block" : "none")};
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 1400;
+`;
+export const IconWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  /* width: 100%; */
+  /* height: 100%; */
+`;
+
+export const MdChevronRightStyled = styled(MdChevronRight)`
+  color: ${({ theme }) => theme.color.primary};
+  /* width: 24px;
+  height: 24px; */
+`;
+
+export const MdChevronLeftStyled = styled(MdChevronLeft)`
+  color: ${({ theme }) => theme.color.primary};
+  /* width: 24px;
+  height: 24px; */
+`;
